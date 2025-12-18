72338d24b5f0b337b5807cf5540f105287cf2177
mriyani <ryaneq@gmail.com>
2025-12-18 11:52:27 +0800

    Echo syntax

---
+++ 2-echoSyntax.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Echo</title>
+
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            /* height: 100vh; */
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1><?php echo "Hello, World!"; ?></h1>
+    <h1><?= "Hello, World!"; ?></h1>
+
+</body>
+
+</html>
