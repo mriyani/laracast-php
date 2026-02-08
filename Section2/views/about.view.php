b58dab36c027f5ed82c68b27c5d475251b29f70f
mriyani <ryaneq@gmail.com>
2026-02-08 23:30:38 +0800

    Superglobals and current page styling

--- Section2/views/about.view.php
+++ Section2/views/about.view.php
@@ -1,7 +1,5 @@
 <?php require('partials/header.php'); ?>
-
 <?php require('partials/nav.php'); ?>
-
 <?php require('partials/banner.php'); ?>
 
 <main>
--- Section2/views/partials/nav.php
+++ Section2/views/partials/nav.php
@@ -8,9 +8,9 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                        <a href="/Section2/" aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</a>
-                        <a href="/Section2/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                        <a href="/Section2/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                        <a href="/Section2/" aria-current="page" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/Section2/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Home</a>
+                        <a href="/Section2/about.php" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/Section2/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">About</a>
+                        <a href="/Section2/contact.php" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/Section2/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Contact</a>
                     </div>
                 </div>
             </div>
